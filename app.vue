cde3d490658b733a0cc46e46d2f214ec4e409bc3
samardzievm <samardzievm@gmail.com>
2023-01-17 19:57:57 +0100

    Index page almost complete.

--- app.vue
+++ app.vue
@@ -1,7 +1,63 @@
 <template>
-  <div>
-    <div class="flex h-screen justify-center place-items-center">
-      <h1 class="text-2xl fond-bold">Hello World</h1>
+  <div class="bg-gray-800 h-screen">
+    <!-- container-->
+    <div class="flex justify-center bg-gray-800 py-12">
+      <!-- center content-->
+      <div class="text-center">
+        <!-- title-->
+        <h1 class="my-3 text-2xl fond-bold text-white">Bookoholic - Your Movie Guru</h1>
+        <!-- end title-->
+
+        <!-- user interaction-->
+        <div class="flex justify-center">
+          <input type="text"
+                 v-model="userTextInput"
+                 minlength="10"
+                 placeholder="Search Title..."
+                 class="bg-gray-50 h-10 px-3 lg:w-72"
+          />
+          <button class="bg-yellow-400 text-black font-semibold w-16 h-10 ml-1"
+                  @click="handleResetButton()">Reset
+          </button>
+        </div>
+
+        <!-- end user interaction-->
+
+        <!-- list of book cards-->
+        <div v-show="!userTextInput.value" class="mt-5 px-8 w-full lg:3/4 2xl:w-2/3 m-auto
+                    grid grid-flow-row-dense grid-cols-1 lg:grid-cols-5 gap-8">
+          <BookCard v-show="typeof book.volumeInfo.averageRating !== 'undefined'"
+                    v-for="book in books.items" :key="book"
+                    :book="book.volumeInfo"
+          />
+        </div>
+        <!-- end list of book cards-->
+      </div>
+      <!-- end center content-->
     </div>
+    <!-- end container-->
   </div>
 </template>
+
+<script setup>
+import BookCard from "~/components/BookCard.vue";
+
+// conflict is good title to search for because all ratings are displayed (from 1 to 5)
+let userTextInput = ref("conflict");
+
+const booksApiUrl = 'https://www.googleapis.com/books/v1/volumes?q=intitle:';
+
+function handleResetButton() {
+  userTextInput.value = "";
+}
+
+const {data: books} = await useAsyncData(() =>
+    $fetch(`${booksApiUrl}${userTextInput.value}&orderBy=relevance`), {
+  watch: [userTextInput]
+});
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/BookCard.vue
@@ -0,0 +1,60 @@
+<template>
+  <div v-if="book" class="p-4 bg-gray-50 rounded-lg select-none"
+       :class="columnSizeByRatingClass(Number(book.averageRating))">
+    <!-- inside card content-->
+    <div class="flex flex-col justify-between h-full">
+      <!-- top content-->
+      <div class="">
+        <!-- image -->
+        <img :src="book.imageLinks.thumbnail" :alt="`${book.title} Image`"
+             class="m-auto w-full h-fit">
+        <!-- end image-->
+
+        <!-- book title-->
+        <h4 class="font-bold hover:underline pt-3">
+          <a href="#">{{ book.title }}</a>
+        </h4>
+        <!-- end book title-->
+      </div>
+      <!-- top content-->
+
+      <!-- description-->
+      <div class="py-3">
+        <p class="text-sm text-justify">{{ book.description }}</p>
+      </div>
+      <!--  end description-->
+
+      <!-- bottom content-->
+      <div class="flex justify-between place-items-center">
+        <!-- rating-->
+        <p class="font-bold text-sm">
+          {{ book.averageRating }}{{ book.averageRating ? '&#9733;' : '' }}
+        </p>
+        <!-- end rating-->
+
+        <!-- link to details page-->
+        <a href="#" class="text-white px-3 py-1 text-sm fond-semibold bg-red-700 hover:bg-red-600 rounded-r-lg">
+          Learn More
+        </a>
+        <!-- end link to details page-->
+      </div>
+      <!-- end bottom-->
+    </div>
+    <!-- end inside card content-->
+  </div>
+</template>
+
+<script setup>
+defineProps(['book'])
+
+function columnSizeByRatingClass(rating) {
+  if (rating >= 4.0) return "lg:col-span-2 lg:row-span-2";
+  else if (rating >= 2.0 && rating < 4.0) return "lg:col-span-2";
+  else if (rating < 2.0) return "lg:col-span-1 lg:row-span-1";
+  else return "";
+}
+</script>
+
+<style scoped>
+
+</style>
